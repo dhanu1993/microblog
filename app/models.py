c3005b4bb7a7b94d71bf0a2ffba0490e1f6dae69
Dhanunjaya Panchireddy <dhanunjaya.panchireddy@gmail.com>
2018-03-13 22:12:12 +0530

    add

--- app/models.py
+++ app/models.py
@@ -1,18 +1,37 @@
-from app import db
-from datetime import datetime 
+from app import db, login
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash 
+from flask_login import UserMixin
+from hashlib import md5
 
-class User(db.Model):
+class User(UserMixin, db.Model):
 	__tablename__="users"
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(64), unique=True, index=True)
 	email = db.Column(db.String(120), unique=True, index=True)
 	password_hash=db.Column(db.String(128))
 	posts = db.relationship('Post', backref='author', lazy='dynamic')
+	about_me=db.Column(db.String(140))
+	last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
 	def __repr__(self):
 		return "<user : {}>.".format(self.username)
 
-class Post(db.Model):
+	def set_password(self, password):
+		self.password_hash=generate_password_hash(password)
+
+	def check_password(self, password):
+		return check_password_hash(self.password_hash, password)
+
+	# def avatar(self, size):
+	# 	return 'https://secure.gravatar.com/avatar/b9ef30a21e4caab9eec3997f45048701?s={}'.format(size)
+
+	def avatar(self,size):
+		digest=md5(self.email.lower().encode('utf-8')).hexdigest()
+		return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+
+
+class Post(UserMixin, db.Model):
 	__tablename__='posts'
 	id=db.Column(db.Integer, primary_key=True)
 	body=db.Column(db.String(140))
@@ -21,3 +40,7 @@ class Post(db.Model):
 
 	def __repr__(self):
 		return "<post {}>".format(self.body)
+
+@login.user_loader
+def load_user(id):
+	return User.query.get(int(id))
