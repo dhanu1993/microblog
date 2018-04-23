a6f0644926147e2026e9849e8f6e67fd03351c6a
Dhanunjaya Panchireddy <dhanunjaya.panchireddy@gmail.com>
2018-04-23 15:08:41 +0530

    init

--- microblog.py
+++ microblog.py
@@ -1,7 +1,11 @@
 from app import app, db
 from app.models import User, Post
+import os
 
 @app.shell_context_processor 
 def make_shell_context():
 	return {'db':db, 'User':User, 'Post':Post}
 
+port  = int(os.environ.get('PORT', 5000))
+
+app.run(host='0.0.0.0', port=port)
